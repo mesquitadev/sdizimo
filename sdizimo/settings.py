a8ee7de392cb56b19acb1c334e06826dc6ab25a6
Daniel Lima <danielslz@gmail.com>
2017-10-15 17:44:06 -0300

    Criada estrutura básica de templates.

--- sdizimo/settings.py
+++ sdizimo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # project apps
+    'apps.comum',
     'apps.dizimo',
 ]
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'sdizimo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'apps/comum/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
--- sdizimo/urls.py
+++ sdizimo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from apps.comum import views
 
 urlpatterns = [
+    url(r'^$', views.dashboard, name='dashboard'),
     url(r'^admin/', admin.site.urls),
 ]
