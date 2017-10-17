12d310bc8fccd7e8f2515c7acde4bab98fb93e28
Daniel Lima <daniel.lima@ifma.edu.br>
2017-10-17 11:43:35 -0300

    Início da configuração do datepicker.

--- apps/dizimo/forms.py
+++ apps/dizimo/forms.py
@@ -1,7 +1,10 @@
+from bootstrap_datepicker.widgets import DatePicker
 from django import forms
 from .models import Dizimista
 
 class DizimistaForm(forms.ModelForm):
+    data_nascimento = forms.DateField(widget=DatePicker(options={"autoclose": True}))
+
     class Meta:
         model = Dizimista
         fields = '__all__'
