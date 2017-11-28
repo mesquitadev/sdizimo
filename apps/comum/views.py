4c81de86ed59cf1980e8648e6bc7f918ec576470
Daniel Lima <daniel.lima@ifma.edu.br>
2017-11-28 09:27:17 -0300

    Colocados dados estatísticos na página inicial.

--- apps/comum/views.py
+++ apps/comum/views.py
@@ -1,10 +1,29 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
+
+from apps.dizimo.models import Dizimista, Dizimo, Oferta, Batismo, Doacao
 
 
 @login_required
 def inicio(request):
+    qtd_dizimistas = Dizimista.objects.count()
+    qtd_dizimos = Dizimo.objects.count()
+    qtd_ofertas = Oferta.objects.count()
+    qtd_batismos = Batismo.objects.count()
+    qtd_doacoes = Doacao.objects.count()
+    qtd_usuarios = User.objects.count()
+
+    qtd_recebimentos = qtd_dizimos + qtd_batismos + qtd_doacoes + qtd_ofertas
+
     context = {
-        'menu': 'inicio'
+        'menu': 'inicio',
+        'qtd_dizimistas': qtd_dizimistas,
+        'qtd_dizimos': qtd_dizimos,
+        'qtd_ofertas': qtd_ofertas,
+        'qtd_batismos': qtd_batismos,
+        'qtd_doacoes': qtd_doacoes,
+        'qtd_usuarios': qtd_usuarios,
+        'qtd_recebimentos': qtd_recebimentos
     }
     return render(request, 'inicio.html', context)
