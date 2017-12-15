0d415ae5a9c1804e88f6c049498f91c53dd62832
Daniel Lima <daniel.lima@ifma.edu.br>
2017-12-15 11:30:10 -0300

    Correção das máscaras de telefone.

--- static/js/app.js
+++ static/js/app.js
@@ -7,3 +7,14 @@ jQuery(context).find(".confirm, .danger, .icon-trash, .delete").not(".no-confirm
     e.preventDefault();
   }
 });
+
+var phoneMaskBehavior = function (val) {
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+};
+
+var phoneMaskOptions = {
+  placeholder: "(__) _____-____",
+  onKeyPress: function(val, e, field, options) {
+      field.mask(phoneMaskBehavior.apply({}, arguments), options);
+    }
+};
