fe34f92de46616bbe488a4edcc6873178fef2628
Daniel Lima <danielslz@gmail.com>
2018-08-25 23:44:08 -0300

    Alterada página inicial para exibir dados do mês corrente.

--- apps/comum/views.py
+++ apps/comum/views.py
@@ -1,23 +1,23 @@
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.shortcuts import render
 
-from apps.dizimo.models import Dizimista, Dizimo, Oferta, Batismo, Doacao
+from apps.dizimo.models import Dizimista, Dizimo, Oferta, Batismo, Doacao, Pagamento
 
 
 @login_required
 def inicio(request):
+    mes_atual = datetime.today().month
     qtd_dizimistas = Dizimista.objects.count()
-    qtd_dizimos = Dizimo.objects.count()
-    qtd_ofertas = Oferta.objects.count()
-    qtd_batismos = Batismo.objects.count()
-    qtd_doacoes = Doacao.objects.count()
-    qtd_usuarios = User.objects.count()
+    qtd_dizimos = Dizimo.objects.filter(cadastrado_em__month=mes_atual).count()
+    qtd_ofertas = Oferta.objects.filter(cadastrado_em__month=mes_atual).count()
+    qtd_batismos = Batismo.objects.filter(cadastrado_em__month=mes_atual).count()
+    qtd_doacoes = Doacao.objects.filter(cadastrado_em__month=mes_atual).count()
+    qtd_pagamentos = Pagamento.objects.filter(cadastrado_em__month=mes_atual).count()
 
     qtd_recebimentos = qtd_dizimos + qtd_batismos + qtd_doacoes + qtd_ofertas
 
-    qtd_aniversariantes = Dizimista.objects.filter(data_nascimento__month=datetime.today().month).count()
+    qtd_aniversariantes = Dizimista.objects.filter(data_nascimento__month=mes_atual).count()
 
     context = {
         'menu': 'inicio',
@@ -26,7 +26,7 @@ def inicio(request):
         'qtd_ofertas': qtd_ofertas,
         'qtd_batismos': qtd_batismos,
         'qtd_doacoes': qtd_doacoes,
-        'qtd_usuarios': qtd_usuarios,
+        'qtd_pagamentos': qtd_pagamentos,
         'qtd_recebimentos': qtd_recebimentos,
         'qtd_aniversariantes': qtd_aniversariantes
     }
