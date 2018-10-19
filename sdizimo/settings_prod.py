60e6faa5bd37475e6afe29765925eb381185db6e
Daniel Lima <danielslz@gmail.com>
2018-10-18 22:27:41 -0300

    Desabilitado log

--- sdizimo/settings_prod.py
+++ sdizimo/settings_prod.py
@@ -47,65 +47,65 @@ SESSION_COOKIE_AGE = 60 * 45  # 45m, in seconds
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 
-LOGS_ROOT = '/home/infobits/logs'
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'formatters': {
-        'simple': {
-            'format': '[%(asctime)s] %(levelname)s %(message)s',
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'verbose': {
-            'format': '[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s',
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'development_logfile': {
-            'level': 'DEBUG',
-            'filters': ['require_debug_true'],
-            'class': 'logging.FileHandler',
-            'filename': LOGS_ROOT + '/sdizimo_dev.log',
-            'formatter': 'verbose'
-        },
-        'production_logfile': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': LOGS_ROOT + '/sdizimo_prod.log',
-            'maxBytes': 1024*1024*100,  # 100MB
-            'backupCount': 5,
-            'formatter': 'simple'
-        },
-    },
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['console'],
-    },
-    'loggers': {
-        'sdizimo': {
-            'handlers': ['development_logfile', 'production_logfile'],
-         },
-        'django': {
-            'handlers': ['development_logfile', 'production_logfile'],
-        },
-        'py.warnings': {
-            'handlers': ['development_logfile'],
-        },
-    }
-}
+# LOGS_ROOT = '/home/infobits/logs'
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'filters': {
+#         'require_debug_false': {
+#             '()': 'django.utils.log.RequireDebugFalse',
+#         },
+#         'require_debug_true': {
+#             '()': 'django.utils.log.RequireDebugTrue',
+#         },
+#     },
+#     'formatters': {
+#         'simple': {
+#             'format': '[%(asctime)s] %(levelname)s %(message)s',
+#             'datefmt': '%Y-%m-%d %H:%M:%S'
+#         },
+#         'verbose': {
+#             'format': '[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s',
+#             'datefmt': '%Y-%m-%d %H:%M:%S'
+#         },
+#     },
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'filters': ['require_debug_true'],
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'simple'
+#         },
+#         'development_logfile': {
+#             'level': 'DEBUG',
+#             'filters': ['require_debug_true'],
+#             'class': 'logging.FileHandler',
+#             'filename': LOGS_ROOT + '/sdizimo_dev.log',
+#             'formatter': 'verbose'
+#         },
+#         'production_logfile': {
+#             'level': 'ERROR',
+#             'filters': ['require_debug_false'],
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'filename': LOGS_ROOT + '/sdizimo_prod.log',
+#             'maxBytes': 1024*1024*100,  # 100MB
+#             'backupCount': 5,
+#             'formatter': 'simple'
+#         },
+#     },
+#     'root': {
+#         'level': 'DEBUG',
+#         'handlers': ['console'],
+#     },
+#     'loggers': {
+#         'sdizimo': {
+#             'handlers': ['development_logfile', 'production_logfile'],
+#          },
+#         'django': {
+#             'handlers': ['development_logfile', 'production_logfile'],
+#         },
+#         'py.warnings': {
+#             'handlers': ['development_logfile'],
+#         },
+#     }
+# }
