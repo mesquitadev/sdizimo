67d6142d3b41e78da88711d30a5b34b9ba505e9d
Daniel Lima <danielslz@gmail.com>
2018-10-18 21:42:48 -0300

    Adicionada configuração de logs.

--- sdizimo/settings_prod.py
+++ sdizimo/settings_prod.py
@@ -45,3 +45,51 @@ SERVER_EMAIL = 'sdizimo@infobits.net.br'
 
 SESSION_COOKIE_AGE = 60 * 45  # 45m, in seconds
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+
+LOGS_ROOT = '/home/infobits/sdizimo_logs'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': LOGS_ROOT + "/logfile",
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'sdizimo': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+        },
+    }
+}
